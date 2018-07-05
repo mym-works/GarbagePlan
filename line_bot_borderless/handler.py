c17a8140fc76d33e96e9220b9025379b16c95fbf
mym <k-mayama@MacBook-Pro.local>
2018-07-05 19:42:55 +0900

    LINE投稿する処理を関数化

--- line_bot_borderless/handler.py
+++ line_bot_borderless/handler.py
@@ -1,25 +1,33 @@
-import urllib.request, json, os
+import urllib.request
+import json
+import os
+
 
 def line_bot_response(event, context):
-  access_token = os.environ['ACCESS_TOKEN']
-  group_id = os.environ['GROUP_ID']
-    
-  url = "https://api.line.me/v2/bot/message/push" 
-  method = "POST"
-  headers = {"Content-Type" : "application/json", "Authorization": 'Bearer ' + access_token }
+    post_to_line()
 
-  # PythonオブジェクトをJSONに変換する
-  obj = {
-    'to': group_id,
-    'messages': [{
-      'type': 'text',
-      'text': "Hello World",
-    }],
-  }
-  json_data = json.dumps(obj).encode("utf-8")
 
-  # httpリクエストを準備してPOST
-  request = urllib.request.Request(url, data=json_data, method=method, headers=headers)
-  with urllib.request.urlopen(request) as response:
-      response_body = response.read().decode("utf-8")
+def post_to_line():
+    access_token = os.environ['ACCESS_TOKEN']
+    group_id = os.environ['GROUP_ID']
 
+    url = "https://api.line.me/v2/bot/message/push"
+    method = "POST"
+    headers = {"Content-Type": "application/json",
+               "Authorization": 'Bearer ' + access_token}
+
+    # PythonオブジェクトをJSONに変換する
+    obj = {
+        'to': group_id,
+        'messages': [{
+            'type': 'text',
+            'text': "Hello World",
+        }],
+    }
+    json_data = json.dumps(obj).encode("utf-8")
+
+    # httpリクエストを準備してPOST
+    request = urllib.request.Request(
+        url, data=json_data, method=method, headers=headers)
+    with urllib.request.urlopen(request) as response:
+        response_body = response.read().decode("utf-8")
