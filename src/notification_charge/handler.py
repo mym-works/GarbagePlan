d5a659cacab3b8a8c5bebdf379267a8bbcaace92
KoheiMayama <mym.kohei@gmail.com>
2020-02-23 19:19:32 +0900

    handlerを変更

--- src/notification_charge/handler.py
+++ src/notification_charge/handler.py
@@ -1,39 +1,71 @@
-import urllib.request
 import json
 import os
 import database
 import calc
+import post
 import logging
+from time import sleep
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
 def main(event, context):
-    room_members = database.items_read('borderless-house-members')
-    logger.info(room_members)
+    # ハウス名とLINE GroupIDを取得
+    house_items = database.items_scan('borderles-line-groupid')
 
-    this_week_charger = database.items_read(
-        'borderles-this-week-garbage-charge')
-    logger.info(this_week_charger)
+    # 各ハウスのゴミ捨ての担当とゴミ種類を通知
+    for item in house_items:
+        house_name = item['House']
+        line_groupid = item['GroupId']
 
-    type_of_garbage = database.items_read('borderless-type-of-garbage')
-    logger.info(type_of_garbage)
+        # House memeberを取得
+        house_info = database.items_read(
+            'borderless-house-members', house_name)
+        # logger.info(house_info)
 
-    # 今週のゴミ当番がいるか確認
-    database_charge_room_array, correc_room, correc_name, next_room, next_name = calc.confirm_charge(
-        room_members, this_week_charger)
+        # ごみ捨て当番を取得
+        # 配列の最初の二人が担当
+        # 存在しなかった場合は次の部屋番号
+        people_in_charge = database.items_read(
+            'borderles-people-in-charge', house_name)
+        people_in_charge_queue = people_in_charge[0]['Room']
+        # logger.info(people_in_charge_queue)
 
-    # ゴミの種類を取得
-    todays_garbage_name = calc.confirm_garbage_type(type_of_garbage)
+        # 当日のごみ捨て当番を探索
+        todays_people_in_charge, new_people_in_charge_queue = calc.get_todays_people_in_charge(
+            house_info, people_in_charge_queue)
+        logger.info(todays_people_in_charge)
+        logger.info(new_people_in_charge_queue)
+
+        # ハウスに存在するゴミの種類を取得
+        type_of_garbage = database.items_read(
+            'borderless-type-of-garbage', house_name)
+        # logger.info(type_of_garbage)
+
+        # 当日捨てるゴミの種類を取得
+        todays_garbage_name = calc.get_todays_garbage_type(type_of_garbage)
+        logger.info(todays_garbage_name)
+
+        # ゴミの種類の配列が空ではない場合
+        if todays_garbage_name:
+            # 担当者を通知
+            send_message = calc.make_flex_message_charge(
+                line_groupid, todays_people_in_charge)
+            post.line(send_message)
+
+            sleep(1)
+
+            # ゴミの種類を通知
+            send_message = calc.make_flex_message_garbage(
+                line_groupid, todays_garbage_name)
+            post.line(send_message)
+
+
+"""
 
     # ゴミの種類の配列が空ではない場合
     if todays_garbage_name:
-        # メッセージを整形
-        send_message = calc.make_garbage_massege(
-            correc_name, correc_room, todays_garbage_name)
-        # メッセージをLINEに送信
-        post_to_line(send_message)
 
         # 一旦今週のごみ捨て当番のレコードを削除する
         database.items_delete(
@@ -42,29 +74,4 @@ def main(event, context):
         # 来週のごみ捨て当番のレコードを登録する
         database.items_add('borderles-this-week-garbage-charge',
                            'C5ac0fe9390c99fe1e7aa307168695d04', next_room, next_name)
-
-
-def post_to_line(send_message):
-    access_token = os.environ['ACCESS_TOKEN']
-    group_id = os.environ['GROUP_ID']
-
-    url = "https://api.line.me/v2/bot/message/push"
-    method = "POST"
-    headers = {"Content-Type": "application/json",
-               "Authorization": 'Bearer ' + access_token}
-
-    # PythonオブジェクトをJSONに変換する
-    obj = {
-        'to': group_id,
-        'messages': [{
-            'type': 'text',
-            'text': send_message,
-        }],
-    }
-    json_data = json.dumps(obj).encode("utf-8")
-
-    # httpリクエストを準備してPOST
-    request = urllib.request.Request(
-        url, data=json_data, method=method, headers=headers)
-    with urllib.request.urlopen(request) as response:
-        response_body = response.read().decode("utf-8")
+"""
