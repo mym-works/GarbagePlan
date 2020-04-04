2c664019153db6fd227ead4672acbf47576f8bf4
KoheiMayama <mym.kohei@gmail.com>
2020-04-04 17:47:49 +0900

    ハウスメイトを取得するAPIを追加

---
+++ src/api/get-house-member/handler.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import json
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+
+dynamodb = boto3.resource('dynamodb')
+
+
+def items_scan(house_name):
+    dynamodb_table = dynamodb.Table('borderless-house-members')
+    response = dynamodb_table.scan(
+        FilterExpression=Key('House').eq(house_name)
+    )
+    return response['Items']
+
+
+def main(event, context):
+    event_path = event['path']
+    house_name = (event_path.split('/'))[-1]
+
+    house_member = items_scan(house_name.capitalize())
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(house_member)
+    }
+
+    return response
